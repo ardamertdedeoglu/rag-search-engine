daaa36a8423f27365388fb0dd425eb84d083856f
Arda Mert Dedeoğlu <mert.dedeoglu@std.yildiz.edu.tr>
2026-07-19 12:16:05 +0300

    initial commit - added keyword search

---
+++ cli/lib/keyword_search.py
@@ -0,0 +1,52 @@
+import string
+
+from .search_utils import DEFAULT_SEARCH_LIMIT, STOPWORDS_PATH, load_movies
+from nltk.stem import PorterStemmer
+
+
+
+
+def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
+    movies = load_movies()
+    results = []
+    for movie in movies:
+        query_tokens = tokenize_text(query)
+        title_tokens = tokenize_text(movie["title"])
+        if has_matching_token(query_tokens, title_tokens):
+            results.append(movie)
+            if len(results) >= limit:
+                break
+
+    return results
+
+
+def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
+    for query_token in query_tokens:
+        for title_token in title_tokens:
+            if query_token in title_token:
+                return True
+    return False
+
+
+def preprocess_text(text: str) -> str:
+    text = text.lower()
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
+
+
+def load_stopwords() -> list[str]:
+    with open(STOPWORDS_PATH, "r") as f:
+        return [preprocess_text(word) for word in f.read().splitlines()]
+
+
+STOPWORDS = load_stopwords()
+
+
+def tokenize_text(text: str) -> list[str]:
+    text = preprocess_text(text)
+    tokens = text.split()
+    valid_tokens = [token for token in tokens if token]
+    filtered_words = [word for word in valid_tokens if word not in STOPWORDS]
+    stemmer = PorterStemmer()
+    stemmed_words = [stemmer.stem(x) for x in filtered_words]
+    return stemmed_words
